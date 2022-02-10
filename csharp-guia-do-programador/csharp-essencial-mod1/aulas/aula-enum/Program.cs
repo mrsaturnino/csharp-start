a4e175096c4e0b61925e5813c57c6964b03fb234
André Saturnino <andre_santos2012@hotmail.com>
2022-02-09 22:14:07 -0300

    aulas enum e switch

---
+++ csharp-guia-do-programador/csharp-essencial-mod1/aulas/aula-enum/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace X {
+
+    class Program
+    {
+        //Com o "enum", criamos um conjunto de constantes.
+        enum Cor { Azul, Preto, Vermelho, Verde, Rosa }
+        
+        public static void Main(string[] args)
+        {
+
+        //Ao criarmos este tipo "cor", podemos utilizá-lo dentro de suas limitações.
+        
+        Cor corFavorita = Cor.Azul;
+
+        Console.WriteLine(corFavorita);
+
+        //O enum, como o proprio nome já sugere, enumera os valores definidos. Semelhante às Arrays, o enum começa com [0] [1] ...
+
+        //Alterando um valor, os próximos itens após ele seguirão a mesma linha.
+        //enum Cor  {Azul = 10, Preto (11), Vermelho(12), Rosa(13)}
+
+        }
+    }
+}
+
+
+
+
+
+
---
+++ csharp-guia-do-programador/csharp-essencial-mod1/aulas/aula-switch/Program.cs
@@ -0,0 +1,21 @@
+﻿//O switch funciona praticamente da mesma maneira que as condicionais if, if else e else.
+
+string corFavorita = "Verde"; //Criando uma variável para utilizar nas condições do switch.
+
+switch(corFavorita){ //Abrimos o switch com a variável corFavorita guardando o valor "Verde".
+    
+    case "Vermelho": //Caso este valor guardado na variável seja "Vermelho", será apresentado no console a seguinte frase:
+        Console.WriteLine("Sua cor favorita é vermelho!"); //frase
+    break;
+
+    case "Azul":
+        Console.WriteLine("Sua cor favorita é azul!");
+    break; //O break finaliza o caso após apresentar os comandos acima dele.
+
+    case "Verde":
+        Console.WriteLine("Sua cor favorita é verde!");
+    break;
+    default: //Funciona como o "else". Caso nenhuma das condições acima sejam atingidas, será esta a ser lida pelo console.
+        Console.WriteLine("Não sei qual é a sua cor favorita.");
+    break;
+}
