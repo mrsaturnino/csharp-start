a44aac352b80a71cfc7ec0bf12674cc3e1a4852e
André Saturnino <andre_santos2012@hotmail.com>
2022-02-11 22:43:15 -0300

    Aula while

---
+++ csharp-guia-do-programador/csharp-essencial-mod1/aulas/aula-while/Program.cs
@@ -0,0 +1,9 @@
+﻿int contador = 0; //Começamos com o nosso contador a partir do zero.
+
+while (contador < 20) //Leia: Enquanto a variável contador for menor que 20, faça abaixo:
+{
+    Console.WriteLine(contador); //vai exibir o valor do contador cada vez que a repetição acontecer
+    Console.WriteLine("Rodando o while!");
+    contador = contador + 1; //Será adicionado +1 a cada repetição. Chegando até 20, o console irá imprimir "Fim do While"
+}
+Console.WriteLine("Fim do While!");
