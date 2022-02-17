6f8bb27439574a5deb040abbf5a730e5a3568e88
André Saturnino <andre_santos2012@hotmail.com>
2022-02-16 23:37:50 -0300

    aula for

---
+++ csharp-guia-do-programador/csharp-essencial-mod1/aulas/aula-for/Program.cs
@@ -0,0 +1,24 @@
+﻿//for (contador; condição; incremento ou decremento){}
+//É uma estrutura de repetição muito utilizada, principalmente com arrays!
+
+
+for(int contador = 0; contador < 10; contador++)
+{
+    Console.WriteLine("Este é o for rodando!");
+}
+Console.WriteLine("==========================");
+Console.WriteLine();
+
+
+//Percorrendo arrays com o for
+
+string[] hobbies = {"Viajar", "Ler", "Plantas", "Corrida", "Games"};
+
+for(int contador3 = 0; contador3 >= 0; contador3 --) //arrays começam sempre da partir do zero.
+{
+    Console.WriteLine("essa é uma forma legal de acessar as máquinas.");
+}
+
+    /*Aqui, começamos o contador a partir do ultimo valor da samsung
+    for int contador = 3; contador > = 0; contador--)
+    */
