8620e2118a9dd423caac69fa05bf30ffae714810
André Saturnino <andre_santos2012@hotmail.com>
2022-02-20 21:54:32 -0300

    Inicio do projeto

---
+++ csharp-guia-do-programador/csharp-essencial-mod1/listas-de-exercicios/exercícios/ex-repeticoes/ex-003/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Crie um programa C# que exiba para os clientes de um mercado que está aberto
+sob a condição do seu horário de funcionamento das 7h às 17h após esse horário
+exibir uma mensagem de que se encontra fechado e indicando o horário de
+funcionamento deste estabelecimento.
+*/
+
+namespace ExercicioRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============================================");
+            Console.WriteLine("=========== Mercadinho Mau-Preço ===========");
+            Console.WriteLine("================ 7h às 17h =================");
+
+            for (int horario = 7; horario <= 17 ; horario++)
+            {
+                Console.WriteLine($"Estamos funcionando!\n Agora são {horario}h.");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Estamos fechados.\n Volte amanhã!");
+        }
+    }//developed by mrsaturnino
+}
---
+++ csharp-guia-do-programador/csharp-essencial-mod1/projeto01/calculadora-cmd/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Cal
+{
+    class Program
+    {
+        enum Menu { Soma = 1, Subtracao, Divisao, Multiplicacao, Potencia, Raiz, Sair }
+
+        static void Main(string[] args)
+        {
+
+
+            bool escolheuSair = false;
+            while(!escolheuSair)
+            {
+                Console.WriteLine("Olá, seja bem-vindo ao Cal!, Selecione uma das opções abaixo: ");
+                Console.WriteLine("1-Soma\n2-Subtracao\n3-Divisao\n4-Multiplicacao\n5-Potencia\n6-Raiz\n7-Sair");
+
+                Menu opcao = (Menu)int.Parse(Console.ReadLine());
+                
+                switch (opcao)
+                {
+                    case Menu.Sair:
+                        escolheuSair = true;
+                    break;
+                }
+
+                Console.Clear();
+            }
+        }
+    }
+}
