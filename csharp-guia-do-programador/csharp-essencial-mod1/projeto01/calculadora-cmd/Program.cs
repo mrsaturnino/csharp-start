2df094d34913fb66921533b4a1d01e8fc724976a
André Saturnino <andre_santos2012@hotmail.com>
2022-02-22 22:14:37 -0300

    Soma e Subtracao - Projeto calculadora

--- csharp-guia-do-programador/csharp-essencial-mod1/projeto01/calculadora-cmd/Program.cs
+++ csharp-guia-do-programador/csharp-essencial-mod1/projeto01/calculadora-cmd/Program.cs
@@ -21,10 +21,54 @@
                     case Menu.Sair:
                         escolheuSair = true;
                     break;
+                    
+                    case Menu.Soma:
+                        Soma();
+                    break;
+
+                    case Menu.Subtracao:
+                        Sub();
+                    break;
                 }
 
                 Console.Clear();
             }
+        }
+
+        static void Soma()
+        {
+            Console.WriteLine("Soma entre dois números");
+
+            Console.WriteLine("Digite o primeiro número: ");
+            int a = int.Parse(Console.ReadLine());   
+            
+            Console.WriteLine("Digite o segundo número: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int result = a + b;
+
+            Console.WriteLine($"O resultado da soma é: {result}"); 
+            
+            Console.WriteLine($"Aperte ENTER para voltar ao menu.");
+            Console.ReadLine();
+
+        }
+        static void Sub()
+        {
+            Console.WriteLine("Subtração entre dois números");
+
+            Console.WriteLine("Digite o primeiro número: ");
+            int a = int.Parse(Console.ReadLine());   
+            
+            Console.WriteLine("Digite o segundo número: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int result = a - b;
+            
+            Console.WriteLine($"O resultado da subtração é: {result}");
+            
+            Console.WriteLine($"Aperte ENTER para voltar ao menu.");
+            Console.ReadLine();
         }
     }
 }
