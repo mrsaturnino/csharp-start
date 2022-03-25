645ffa2a1538f28a3c98420f9c6a4d048b38e856
André Saturnino <andre_santos2012@hotmail.com>
2022-03-25 18:45:14 -0300

    Adicionando e removendo itens em listas

---
+++ csharp-guia-do-programador/mod_3_csharp-listas/aulas/aula_1_criandoListas/aula_1_criando_listas_e_null/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic; //Adiciona: Listas, Dicionários, Sets, HashTables e várias outras estruturas de dados;
+using System.Linq; //Manipular conjuntos de dados C#; Funcionalidades como filtragem, remoção, busca, e muitas outras.
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_1_criando_listas_e_null
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> clientes = new List<string>(); 
+            /* 
+             O "new List<string>();" cria uma nova lista e coloca dentro da lista clientes,
+             já que tipos complexos, como Listas, não podem ter valores "null".
+             */
+            
+            //Adicionando itens à lista
+
+            clientes.Add("Andre"); //Aqui, adicionamos uma string à nossa lista.
+            clientes.Add("Saturnino"); //Lembre-se sempre de colocar elementos do mesmo tipo da lista.
+            string pessoa = "Pedro";
+            clientes.Add(pessoa); //Também podemos adicionar variáveis (que sejam do msm tipo) às listas!
+
+            Console.WriteLine(clientes[2]); //"Acessamos" as listas da mesma forma que as Arrays.
+
+            clientes [1] = "Clint"; //Alterando itens da lista
+            Console.WriteLine(clientes[1]);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ csharp-guia-do-programador/mod_3_csharp-listas/aulas/aula_2_removendo_itens/aula_2_removendo_itens/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_2_removendo_itens
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> clientes = new List<string>();
+            clientes.Add("Fish");
+            clientes.Add("Mars");
+            clientes.Add("FicaFliston");
+            clientes.Add("Clint");
+            clientes.Add("Guts");
+            string pessoa = "Jotta";
+            clientes.Add(pessoa);
+
+            //Removendo itens de listas!
+
+            foreach (string cliente in clientes) //Um foreach para mostrar cada cliente que está na lista.
+            {
+                Console.WriteLine(cliente);
+            }
+
+            clientes.RemoveAt(1); //Removendo o item de índice "1", ou seja, o "Mars".
+
+            Console.WriteLine("=============================");
+
+            foreach (string cliente in clientes) 
+                //O mesmo foreach, desta vez, mostrando a lista já com o Mars excluído pelo RemoveAt(1);
+            {
+                Console.WriteLine(cliente);
+            }
+
+            //Agora, removendo pelo item em si, e não pelo seu índice.
+
+            clientes.RemoveAll( cliente => cliente == "Fish");
+            /*
+             Basicamente, criamos uma frase para o C#.
+             O RemoveAll vai "varrer" a lista clientes tentando encontrar o "Fish" e irá removê-lo. 
+             Mesmo que ele esteja também em outra posição, o "RemoveAll" só acaba quando termina.
+             */
+
+            Console.WriteLine("=============================");
+
+            foreach (string cliente in clientes) 
+                //O mesmo foreach, desta vez, mostrando a lista já com o Fish excluído pelo RemoveAll();
+            {
+                Console.WriteLine(cliente);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
