1a34a59d4be70f7d4f73d44d5ee6176123f3825c
mrsaturnino <andre_santos2012@hotmail.com>
2022-04-16 11:36:17 -0300

    Introdução a POO

---
+++ csharp-guia-do-programador/mod_5_csharp_poo/aulas/aula01_introducao/aula_introducao/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_introducao
+{
+    class Jogo
+    {
+        //atributos da classe (Dados da classe "jogo")
+        public string titulo;
+        public string genero;
+        public string desenvolvedora;
+        public float preco;
+
+        //métodos da classe (Ações que a classe realiza)
+        
+        public void Abrir() 
+        {
+            Console.WriteLine("Abrindo...");
+        }
+
+        public void Carregar() 
+        { 
+            Console.WriteLine("Loading...");
+        }
+
+        public void Fechar() 
+        { 
+            Console.WriteLine("Aperte ESC para sair...");
+        }
+
+    }
+}
