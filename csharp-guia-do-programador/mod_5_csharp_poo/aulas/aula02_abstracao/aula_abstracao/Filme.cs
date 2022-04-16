8e81dbd27e3916ff0ba07a6b2aa55159095ef22b
mrsaturnino <andre_santos2012@hotmail.com>
2022-04-16 12:31:56 -0300

    Criando classes, seus atributos e métodos

---
+++ csharp-guia-do-programador/mod_5_csharp_poo/aulas/aula02_abstracao/aula_abstracao/Filme.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_abstracao
+{
+    internal class Filme
+    {
+        //definindo atributos/campos da classe
+        public string nome; //"public" para que qualquer classe possa acessar este campo
+        public string desc;
+        public string estudio;
+        public int ano;
+        public List<string> atores = new List<string>();
+
+        //definindo métodos
+
+        public void Executar()
+        {
+            Console.WriteLine($"Reproduzindo: {nome}");
+        }
+
+        public void Pausar()
+        {
+            Console.WriteLine("||");
+        }
+    }
+}
---
+++ csharp-guia-do-programador/mod_5_csharp_poo/aulas/aula02_abstracao/aula_abstracao/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_abstracao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Filme filme = new Filme(); //Chamando a classe "Filme" para criar uma variável que utiliza seus campos/atributos.
+
+            filme.nome = "Titanic"; //Atribuindo um valor a esta variável, que utilizará o campo "nome".
+
+            filme.Executar(); //Após o valor "Titanic" estar dentro da variável, a função "Executar()" fará o seu papel.
+
+            //Criando um segundo filme
+            Console.WriteLine("============================");
+            Filme filme2 = new Filme();
+            filme2.nome = "Pearl Harbor";
+            filme2.Executar();
+
+            Console.ReadLine();
+        }
+    }
+}
