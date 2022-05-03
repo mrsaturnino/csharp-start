3726f28a7d8fc49c483c06f537a63b8aeb25246f
mrsaturnino <andre_santos2012@hotmail.com>
2022-05-02 22:23:24 -0300

    Abstração e classes

---
+++ csharp-guia-do-programador/mod_5_csharp_poo/aulas/aula03_classes/Filme.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula03_classes
+{
+    internal class Filme
+    {
+        //atributos da classe Filme
+        public string title;
+        public string desc;
+        public int year;
+        public string studio;
+        public List<string> actors = new List<string>();
+
+        //métodos da classe Filme
+        public void Executar()
+        {
+            Console.WriteLine($"Playing: {title}");
+        }
+
+        public void Pausar()
+        {
+            Console.WriteLine("||");
+        }
+
+        //A partir disto, temos a estrutura de que precisamos para CRIAR um filme em nossa classe principal, que contém o método "Main". 
+
+    }
+}
---
+++ csharp-guia-do-programador/mod_5_csharp_poo/aulas/aula03_classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula03_classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Filme filme = new Filme();
+            filme.title = "Titanic";
+            filme.Executar();
+
+            Filme filme2 = new Filme();
+            filme.title = "Pearl Harbor";
+            filme.Executar();
+        }
+    }
+}
