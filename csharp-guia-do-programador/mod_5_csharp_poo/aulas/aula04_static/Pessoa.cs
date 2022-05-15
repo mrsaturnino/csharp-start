f9f3db5a9950c27aea34311d953ede34c02ac2c4
mrsaturnino <andre_santos2012@hotmail.com>
2022-05-15 19:32:18 -0300

    Operador "static"

---
+++ csharp-guia-do-programador/mod_5_csharp_poo/aulas/aula04_static/Pessoa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula04_static
+{
+    internal class Pessoa
+    {
+        public string nome;
+        static public string name = "Andre";
+
+        static public void CallName()
+        {
+            Console.WriteLine(name);
+        }
+
+        public void ChamarNome()
+        {
+            Console.WriteLine(nome);
+        }
+    }
+
+    
+}
---
+++ csharp-guia-do-programador/mod_5_csharp_poo/aulas/aula04_static/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula04_static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //campo estático sendo acessado diretamente pela classe.
+            Pessoa.CallName();
+
+            Console.WriteLine();
+
+            //campo não-estático sendo acessado por um objeto
+            Pessoa pessoa = new Pessoa();
+            pessoa.nome = "André";
+            pessoa.ChamarNome();
+        }
+    }
+}
