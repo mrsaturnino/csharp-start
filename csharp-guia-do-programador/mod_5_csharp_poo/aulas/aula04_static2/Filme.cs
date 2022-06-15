a7d328da16c12d556d4a5bc250dcadc328b9cb1b
mrsaturnino <andre_santos2012@hotmail.com>
2022-06-15 06:04:29 -0300

    Static 2

---
+++ csharp-guia-do-programador/mod_5_csharp_poo/aulas/aula04_static2/Filme.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_static2
+{
+    internal class Filme
+    {
+        public string nome;
+        public string desc;
+        public int ano;
+        public string estudio;
+
+        public Filme(string nome, string desc, int ano, string estudio)
+        {
+            this.nome = nome;
+            this.desc = desc;
+            this.ano = ano;
+            this.estudio = estudio;
+        }
+    }
+}
---
+++ csharp-guia-do-programador/mod_5_csharp_poo/aulas/aula04_static2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_static2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o título do filme: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite a descrição do filme: ");
+            string desc = Console.ReadLine();
+            Console.WriteLine("Digite o ano do filme: ");
+            int ano = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o estúdio do filme: ");
+            string estudio = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Filme filme = new Filme(nome, desc, ano, estudio);
+
+            Console.WriteLine($"Título: {filme.nome}");
+            Console.WriteLine($"Descrição do filme: {filme.desc}");
+            Console.WriteLine($"Ano: {filme.ano}");
+            Console.WriteLine($"Estúdio: {filme.estudio}");
+            Console.ReadLine();
+        }
+    }
+}
