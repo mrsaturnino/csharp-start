bf0a9ba0abbe99253871fa05569ae383e0486855
André Saturnino <andre_santos2012@hotmail.com>
2022-06-20 09:47:51 -0300

    Fim do módulo de POO; Structs vs Class

---
+++ csharp-guia-do-programador/mod_5_csharp_poo/aulas/aula09_struct_class/Filme.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula09_struct_class
+
+{
+
+    class Filme
+    {
+        public string nome;
+        public string desc;
+        public int ano;
+        public string estudio;
+
+        public Filme(string nome, string desc, int ano, string estudio) 
+        {
+            this.nome = nome;
+            this.desc = desc;
+            this.ano = ano;
+            this.estudio = estudio;
+        }
+    }
+}
---
+++ csharp-guia-do-programador/mod_5_csharp_poo/aulas/aula09_struct_class/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula09_struct_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Diferente das structs, quando trabalhamos com classes, o objeto que criamos é alocado em algum espaço na memória e então todas as variáveis que referenciamos esse objeto apontam diretamente para o endereço desse espaço alocado.
+
+            Filme filme = new Filme("Topgun", "adkksoakd", 1990, "aaa"); 
+            Filme filme2 = filme; //A variável "filme2" é o que chamamos de "ponteiro", já que esta aponta para o endereço do objeto.
+
+            Console.WriteLine(filme2.nome); //A variável "filme2" está referenciando a var "filme" que possui o endereço da memória para chegar até o objeto.
+
+            Console.ReadLine();
+        }
+    }
+}
